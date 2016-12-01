3be27ecae9375d6f1a49f2c23c045b6072c359cf
orjape95 <orly.penaa@ug.edu.ec>
2016-12-01 00:34:13 -0500

    edicion

---
+++ proyecto web/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Proyecto</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/main.js"></script>
+</head>
+<body>
+			
+	<header>
+		<section class="wrapper">
+			<?php 
+				include 'includes/menu_sesion.php';
+			 ?>
+		</section>
+	</header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="js/jquery.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
